01e3d2541a5ec50325a15e76b92f21d48d32de28
andreivalentin988 <andreivalentin988@gmail.com>
2022-10-09 21:45:01 +0300

    Initial commit

---
+++ login.js
@@ -0,0 +1,56 @@
+const container = document.querySelector(".container"),
+  pwShowHide = document.querySelectorAll(".showHide"),
+  pwFields = document.querySelectorAll(".password"),
+  singUp = document.querySelector(".signup-link"),
+  login = document.querySelector(".login-link"),
+  register = document.querySelector(".registerBt"),
+  user = document.querySelector(".userName");
+
+// show/hide password
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    pwFields.forEach((pwField) => {
+      if (pwField.type === "password") {
+        pwField.type = "text";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye-slash", "uil-eye");
+        });
+      } else {
+        pwField.type = "password";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye", "uil-eye-slash");
+        });
+      }
+    });
+  });
+});
+
+//change from login to register page
+singUp.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+login.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+//set user caracter limit for registration
+register.addEventListener("click", (inputtxt, minlength) => {
+  var field = inputtxt.value;
+  var mnlen = minlength;
+  // var mxlen = maxlength;
+
+  if (field.length > mnlen) {
+    alert("Please input the userid between " + mnlen + " characters");
+    return false;
+  } else {
+    login.addEventListener("click", () => {
+      container.classList.remove("active");
+    });
+    return true;
+  }
+});
+
+//localStorage.setItem
