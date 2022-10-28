7e43a03d9a3ab17ddc4fdc75392e16961cb789da
andreivalentin988 <andreivalentin988@gmail.com>
2022-10-28 19:44:13 +0300

    something I will delete tomorrow :laughing:

---
+++ mainPage.js
@@ -0,0 +1,10 @@
+const addTask = document.querySelector(".button"),
+  profile = document.querySelector(".User");
+
+addTask.addEventListener("load", (e) => {
+  taskButton = e.target;
+  console.log(taskButton);
+  let div = document.createElement("div");
+  div.innerHTML = `
+  fasfa`;
+});
