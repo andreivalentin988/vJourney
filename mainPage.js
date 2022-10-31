dcabec5ff459483485b0a2304d0a053dfca298d7
andreivalentin988 <andreivalentin988@gmail.com>
2022-10-31 22:48:08 +0200

    I think I got this :laughing:

--- mainPage.js
+++ mainPage.js
@@ -1,16 +1,56 @@
-window.addEventListener("load", () => {
-  const addTasks = document.querySelector(".button"),
-    taskSection = document.querySelector("#tasks");
+// window.addEventListener("load", () => {
+//   const addTasks = document.querySelector(".button"),
+//     taskSection = document.querySelector("#tasks");
 
-  addTasks.addEventListener("click", (e) => {
+//   addTasks.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     let element = e.target;
+//     console.log("element");
+//     const taskAppear = document.createElement("div");
+//     taskAppear.classList.add("task");
+//     const taskContent = document.createElement("div");
+//     taskContent.classList.add("content");
+//     taskAppear.appendChild(taskContent);
+//     taskSection.appendChild(taskAppear);
+//   });
+// });
+
+window.addEventListener("load", () => {
+  const form = document.querySelector("#newTaskForm"),
+    input = document.querySelector("#newTaskInput"),
+    input2 = document.querySelector("");
+  taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let element = e.target;
-    console.log("element");
-    const taskAppear = document.createElement("div");
-    taskAppear.classList.add("task");
-    const taskContent = document.createElement("div");
-    taskContent.classList.add("content");
-    taskAppear.appendChild(taskContent);
-    taskSection.appendChild(taskAppear);
+    const task = input.value;
+
+    if (!task) {
+      alert("Please complete the shift form");
+      return;
+    }
+
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const taskContentElement = document.createElement("div");
+    taskContentElement.classList.add("content");
+    taskContentElement.innerText = task;
+
+    const endInput = document.createElement("input");
+    startInput.classList.add("End");
+    startInput.innerText = task;
+
+    const profitInput = document.createElement("input");
+    startInput.classList.add("totalProfit");
+    startInput.innerText = task;
+
+    const startInput = document.createElement("input");
+    startInput.classList.add("summary");
+    startInput.innerText = task;
+
+    taskElement.appendChild(taskContentElement);
+    taskList.appendChild(taskElement);
+    taskContentElement.appendChild(startInput);
   });
 });
